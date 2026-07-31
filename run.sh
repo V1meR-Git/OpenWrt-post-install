cb7b0dc1e8202f55d3151845806c21d5f3194c8c
V1meR-Git <mihalevmihail0@gmail.com>
2026-07-31 20:38:35 +0000

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo "Скачивание основного скрипта..."
+wget -qO /tmp/install.sh "https://raw.githubusercontent.com/V1meR-Git/OpenWrt-post-install/refs/heads/main/install.sh"
+
+if [ $? -ne 0 ]; then
+    echo "Ошибка: не удалось скачать install.sh. Проверьте подключение к интернету."
+    exit 1
+fi
+
+chmod +x /tmp/install.sh
+
+echo "Запуск установки..."
+/tmp/install.sh
+
+rm -f /tmp/install.sh
